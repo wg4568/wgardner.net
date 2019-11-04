02cb9f8d36a2d25512e257df3ab1dc9a003df35a
William Gardner <539362@cuhsd-int.prv>
2019-11-04 14:00:26 -0800

    setup flask app with session

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template, 
+import json
+import database
+
+# Initialize globals
 app = Flask(__name__)
+db = database.Database('database.db', schema='resources/schema.sql')
+
+# Load configuration from file
+with open('resources/config.json') as f:
+    config = json.load(f)
+
+# Generate secure secret key for session encryption
+if config['debug']:
+    app.secret_key = 'super_secret_debug_key'
+    db.debug = True
+else:
+    app.secret_key = secrets.token_hex(config['secret_length'])
 
 @app.route('/')
 def index():
     return 'Hello, world!'
 
-app.run(host='0.0.0.0', port=8000)
+# Launch the application
+app.run(
+    host=config['host'],
+    port=config['port'],
+    debug=config['debug']
+)
